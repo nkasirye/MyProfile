ccfaec3f5a5188bbd83e660eef32f41db71b188c
Nasar <nasar@Nasars-MacBook-Pro.local>
2018-02-18 14:48:37 -0500

    edited

--- index.js
+++ index.js
@@ -1,5 +1,12 @@
 $(document).ready(function(){
-   loadDoc("repoTable", "https://api.github.com/users/tkasozi/repos");
+   loadDoc("repoTable", "https://api.github.com/users/nkasirye/repos");
+   const elm = $('.btn');
+   $(".btn").on("mouseover", function() {
+    $(this).addClass('big-font');
+}).on('mouseleave',()=> {
+  $(elm).removeClass('big-font');
+});
+
 });
 
 function loadDoc(id,url) {
@@ -8,10 +15,11 @@ function loadDoc(id,url) {
       if (this.readyState == 4 && this.status == 200) {
         let data = JSON.parse( this.responseText);
         for(let x = 0; x <= this.responseText.length; ++x){
-            let elm = $("<a></a>").html(data[x].name).addClass("list-group-item")
-            let new_url = "'"+x.html_url+"'";
+            let new_url = data[x].html_url;            
+            let elm = $("<a></a>").html(data[x].name).addClass("list-group-item").attr("href",new_url)
+            .attr("target","blanck");
             console.log(new_url)
-            $("#"+id).append(elm.attr("href",new_url));
+            $("#"+id).append(elm);
         }
       }
     };
